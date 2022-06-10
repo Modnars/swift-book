4018cd0ceb3efa65762b87018f9802714a29a36f
ModnarShen <modnar@foxmail.com>
2022-06-10 21:10:38 +0800

    add auto deploy script on branch:main

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,12 @@ gitbook build
 git checkout gh-pages
 rm -fr ./docs/ && mkdir -p docs/
 cp -r _book/* docs/
+rm -fr ./docs/deploy.sh  # remove the deploy.sh
 git add ./docs/
 status=`git status -s`
 status=${status:0:1}
 echo "git status: $status"
-if [[ $status = "M" || $status = "A" || $status = "D" || $status = "R" ]]; then #有修改的内容
+if [[ $status = "M" || $status = "A" || $status = "D" || $status = "R" ]]; then
     echo "commit change"
     git commit -m "auto commit by deploy.sh"
     git push
