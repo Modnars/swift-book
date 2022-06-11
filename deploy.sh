6df32641da0d2f4c70a663438e6e75c0c5fedf1b
ModnarShen <modnar@foxmail.com>
2022-06-11 13:08:12 +0800

    auto commit by deploy.sh from branch:main

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+git pull --rebase --autostash  # pull the latest contents
+git add .
+status=`git status -s`
+status=${status:0:1}
+echo "git status: ${status}"
+if [[ $status = "M" || $status = "A" || $status = "D" || $status = "R" ]]; then
+    echo "contents changed, committing..."
+    branch=`git symbolic-ref --short HEAD`
+    git commit -m "auto commit by deploy.sh from branch:${branch}"
+    git push
+fi
+
+rm -fr ./_book/
 gitbook build
 git checkout gh-pages
 rm -fr ./docs/ && mkdir -p docs/
@@ -10,8 +23,9 @@ status=`git status -s`
 status=${status:0:1}
 echo "git status: $status"
 if [[ $status = "M" || $status = "A" || $status = "D" || $status = "R" ]]; then
-    echo "commit change"
-    git commit -m "auto commit by deploy.sh"
+    echo "contents changed, committing..."
+    branch=`git symbolic-ref --short HEAD`
+    git commit -m "auto commit by deploy.sh from branch:${branch}"
     git push
 fi
 git checkout main
